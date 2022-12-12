6bffc5933a09390510f169a922d6a4eae618289c
senthil kumar <senthil.gunasekaran@gmail.com>
2022-12-12 09:22:09 +0000

    Commit

---
+++ HelloZitSpace/Book.cs
@@ -0,0 +1,12 @@
+namespace HelloZitSpace;
+
+public class Book
+{
+    public DateOnly PublishedDate { get; set; }
+
+    public string? Name { get; set; }
+
+  
+
+    public int Page { get; set; }
+}
---
+++ HelloZitSpace/Controllers/LibraryController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloZitSpace.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LibraryController: ControllerBase
+{
+    private static readonly string[] BookNames = new[]
+    {
+    "Absalom, Absalom!",
+"A Time to Kill by John Grisham. ",
+"The House of Mirth by Edith Wharton. ",
+"East of Eden by John Steinbeck. ",
+"The Sun Also Rises by Ernest Hemingway. ",
+"Vile Bodies by Evelyn Waugh. ",
+"A Scanner Darkly by Philip K. ",
+"Moab is my Washpot by Stephen Fry"
+
+    };
+
+    private readonly ILogger<LibraryController> _logger;
+
+    public LibraryController(ILogger<LibraryController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<Book> Get()
+    {
+       
+        return Enumerable.Range(1, 5).Select(index => new Book
+        {
+
+            PublishedDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-Random.Shared.Next(2000))),
+            Page = Random.Shared.Next(50, 200),
+            Name = BookNames[Random.Shared.Next(BookNames.Length)]
+        })
+        .ToArray();
+    }
+}
